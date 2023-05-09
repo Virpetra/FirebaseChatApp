9a7c9e954d4d222a7d828d2c38c0e187b72e466f
Virpetra <msaiddede@gmail.com>
2023-05-09 16:28:51 +0300

    Initial Commit

---
+++ FirebaseChatApp/FirebaseChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseChatAppApp.swift
+//  FirebaseChatApp
+//
+//  Created by Mehmet Said Dede on 9.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
